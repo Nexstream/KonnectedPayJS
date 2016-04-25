f2543653f75c1f6ba95f3528bce16f657a17e98b
TJ Woon <tjwoon@appxtream.com>
2016-04-25 09:08:37 +0800

    Bumped version to v1.0.0

--- src/konnectedpay.js
+++ src/konnectedpay.js
@@ -83,7 +83,7 @@ var hashParams = function ()
 
 // Public API ------------------------------------------------------------------
 
-KonnectedPay.VERSION = "1.0.0-alpha1"
+KonnectedPay.VERSION = "1.0.0"
 
 KonnectedPay.requestPayment = function (config, blank)
 {
