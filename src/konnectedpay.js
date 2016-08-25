2dd18f213e6478b283e2f8c8d5da2da91499a1c1
TJ Woon <tj@cloudsky.org>
2016-08-25 16:10:37 +0800

    [Misc] [Fixed] VERSION in SDK was not updated to 1.1.0...

--- src/konnectedpay.js
+++ src/konnectedpay.js
@@ -83,7 +83,7 @@ var hashParams = function ()
 
 // Public API ------------------------------------------------------------------
 
-KonnectedPay.VERSION = "1.0.0"
+KonnectedPay.VERSION = "1.1.1"
 
 KonnectedPay.requestPayment = function (config, blank)
 {
